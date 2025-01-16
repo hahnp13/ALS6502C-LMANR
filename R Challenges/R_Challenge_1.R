c9b31606af52a4a9d310d432414930de491e263a
Hahn <hahnp@ufl.edu>
2025-01-16 09:27:28 -0500

    updated code

--- R Challenges/R_Challenge_1.R
+++ R Challenges/R_Challenge_1.R
@@ -3,8 +3,15 @@ library(tidyverse)
 library(car)
 library(glmmTMB)
 
-#$ Step 1: run the code below and have a look at the dataset orange ####
+# ---------------------------------------------------------
+# Step 1: Background Information #####
+## Healthy orange trees typically produce fruit at 100 cm in circumference
+## A homeowner calls and says their orange tree is 3 years old (1095 days), but isn't fruiting. They didn't measure it.
+## They also said their are some yellow spots on the leaves. 
+## Lucky for you, you have a previous data set you can use to answer their questions.
 
+
+# Step 2: run the code below and have a look at the dataset orange ####
 data("Orange")  ## load Orange dataset from base R
 head(Orange)    ## measurements of circumference on five trees at 7 time points
 
@@ -12,13 +19,6 @@ head(Orange)    ## measurements of circumference on five trees at 7 time points
 ggplot(Orange, aes(x= ???, y= ???))+
   geom_point()+
   theme_bw(base_size = 16)
-
-# ---------------------------------------------------------
-# Step 2: Background Information #####
-## Healthy orange trees typically produce fruit at 100 cm in circumference
-## A homeowner calls and says their orange tree is 3 years old (1095 days), but isn't fruiting. They didn't measure it.
-## They also said their are some yellow spots on the leaves. 
-
 
 # ---------------------------------------------------------
 # Step 3: Build a linear model (and make plot) to answer the following questions: ####
