877ab3778bd43106f6d36dc01063cfe07099b1fb
Hahn <hahnp@ufl.edu>
2025-01-15 21:58:04 -0500

    updated R challenge 1

--- R Challenges/R_Challenge_1.R
+++ R Challenges/R_Challenge_1.R
@@ -1,20 +1,27 @@
-#############################################################################################################
-##### R CHALLENGE 1 - REGRESSON #############################################################################
+# R CHALLENGE 1 - REGRESSON #############################################################################
 library(tidyverse)
 library(car)
 library(glmmTMB)
 
-## run the code below and have a look at the dataset orange
+#$ Step 1: run the code below and have a look at the dataset orange ####
 
 data("Orange")  ## load Orange dataset from base R
 head(Orange)    ## measurements of circumference on five trees at 7 time points
 
-### Background Information ###
+## hint: plot out data to examine (replace ? with variable name)
+ggplot(Orange, aes(x= ???, y= ???))+
+  geom_point()+
+  theme_bw(base_size = 16)
+
+# ---------------------------------------------------------
+# Step 2: Background Information #####
 ## Healthy orange trees typically produce fruit at 100 cm in circumference
 ## A homeowner calls and says their orange tree is 3 years old (1095 days), but isn't fruiting. They didn't measure it.
 ## They also said their are some yellow spots on the leaves. 
 
-## Build a linear model (and make plot) to answer the following questions:
+
+# ---------------------------------------------------------
+# Step 3: Build a linear model (and make plot) to answer the following questions: ####
 ## 1. What circumference should their tree be, on average? (provide an answer to the nearest centimeter)
 
 ## 2. Should their tree be fruiting by now?  
