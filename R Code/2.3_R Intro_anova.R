da01b27658fb454c0f69b87bccf73c0203c3d675
Hahn <hahnp@ufl.edu>
2025-01-20 15:16:39 -0500

    updated 2.3

--- R Code/2.3_R Intro_anova.R
+++ R Code/2.3_R Intro_anova.R
@@ -4,6 +4,7 @@ library(car)         ### load car package, which is helpful for analyzing linear
 library(emmeans)     ### load emmeans package, which is helpful for getting means from linear models
 library(viridis)     ### load viridis package for adding colors to plots
 library(glmmTMB)
+library(broom)
 
 # EXAMPLE FOR CONDUCTING A ONE-WAY ANOVA IN R #################################
 
@@ -21,7 +22,7 @@ hist(d$count) ## histogram of count data. Can we use to check assumption of norm
 
 ## construct linear model to examine the effect of the different sprays on insect counts ####
 ## for a categorical variable (spray with four levels), we are interested in comparing group means
-lm2 <- glmmTMB(count~spray , data=d)  ## lm is a general function that conducts a linear model
+lm2 <- lm(count~spray , data=d)  ## lm is a general function that conducts a linear model
                                       # all the "calculations" are saved in an object we called 'lm2'
 
 
@@ -36,7 +37,7 @@ summary(lm2)   ## summary() will provide the model coefficients (ie. the "guts" 
                 # the coefficients allow you rebuild the means from the linear model equation y~u+Bi
                 # rebuilding the model from the coefficients is not super helpful and the p-values aren't very meaningful
 
-tidy(lm2, conf.int=TRUE) # tidy() to examine coefficients
+tidy(lm2, conf.int=TRUE) # tidy() to examine coefficients (from broom package or broom.mixed if using glmmTMB)
 glance(lm2) # glance at model fit stats
 
 ## examine group means ####
@@ -62,12 +63,18 @@ boxplot(resid(lm2) ~ d$spray)  ## variances should be homogeneous for each group
 
 ## Plot data with means ####
 ## Plot data and add means plus SE from your emmeans. Can change colors, if you'd like.
-em1 <- emmeans(lm1, ~spray) %>% as.data.frame()
+em1 <- emmeans(lm2, ~spray) %>% as.data.frame()
 
 ggplot() + 
   geom_boxplot(data=d, aes(x=spray,y=count), outlier.shape = NA) + 
   geom_jitter(data=d, aes(x=spray,y=count), height=0,width=.1, size=2) + 
   geom_pointrange(data=em1, aes(x=spray, y=emmean, ymin=emmean-SE, ymax=emmean+SE),
-                color="red", size=1.5, linewidth=2) + ##geom_errorbar() call also be used to draw error bars
+                color="purple", size=1.25, linewidth=2) + ##geom_errorbar() call also be used to draw error bars
   theme_bw(base_size = 14)
 
+
+## re-run using glmmTMB ####
+lm2a <- glmmTMB(count~spray , data=d)
+Anova(lm2a) # note X2 instead of F-value. No Sums of Squares.
+summary(lm2a) # almost identical to lm()
+hist(resid(lm2a))
