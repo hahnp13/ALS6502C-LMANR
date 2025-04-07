e3af7af10cae7f4b6c8caa87161d38b35a21fa69
Hahn <hahnp@ufl.edu>
2025-04-07 16:39:15 -0400

    updated 6.1

--- R Code/6.1_GLMM_Poisson_NB.R
+++ R Code/6.1_GLMM_Poisson_NB.R
@@ -9,7 +9,17 @@ library(performance)
 library(MuMIn)
 library(bbmle)
 
+## Insect sprays example ####
+data("InsectSprays")
+InsectSprays$block <- as.factor(rep(c(1,2,3,4,5,6,7,8,9,10,11,12), 6)) # add blocks
 
+d <- InsectSprays %>% filter(spray=='A'|spray=='B'|spray=='C'|spray=='F')
+
+glmm1 <- glmmTMB(count ~ spray + (1|block), data=d, family="poisson")
+summary(glmm1)
+Anova(glmm1)
+
+# Beall webworm data example ####
 ## Load in and read about the beall.webworms dataset. #### 
 ## The variables of interest are the y-count of webworms, 
 ## spray- spray treatment, and lead-lead treatment. Don't worry about the block or other variables for now.
@@ -19,70 +29,84 @@ d1 <- beall.webworms
 ?beall.webworms  ## info about the beall.webworms dataset
 head(d1) ## view data set
 
-## examine plot of data
+## 0. examine plot of data ####
 ggplot(d1, aes(x=spray, y=y, fill=lead)) + geom_violin(scale="width", adjust=2) + 
   geom_point(position = position_jitterdodge(jitter.width=.5, jitter.height=.1, dodge.width = 1), alpha=.1)+
   facet_wrap(~block)
 
-# implement poisson and negative binomial models ####
+## 1. construct poisson and negative binomial models ####
 #### Models r1 and r2 were used previously in module 3.3
 
 r1 <- glmmTMB(y ~ spray * lead, data=d1, family="poisson")
-summary(r1)
-Anova(r1)
-emmeans(r1, ~spray:lead, type='response') 
+r2 <- glmmTMB(y ~ spray * lead, data=d1, family="nbinom2")
 
-### test for overdispersion
+## 2. Examine residuals and test for overdispersion
+plot(simulateResiduals(r1)) ## DHARMa package simulated residuals
+hist(simulateResiduals(r1)) ## histogram should be flat
 check_overdispersion(r1) # overdispersion ratio calculator from performance
 
-#### model using negative binomial
-r2 <- glmmTMB(y ~ spray * lead, data=d1, family="nbinom2")
-Anova(r2)
-emmeans(r2, ~spray:lead, type='response') 
-
-#### simulate residuals for poisson and negative binomial models
-
-plot(simulateResiduals(r1))
-hist(simulateResiduals(r1)) ## histogram should be flat
-
-plot(simulateResiduals(r2))
+plot(simulateResiduals(r2)) ## DHARMa package simulated residuals
 hist(simulateResiduals(r2)) ## histogram should be flat
-
+check_overdispersion(r2) # overdispersion ratio calculator from performance
 
 
 # QUESTIONS: What's next? Any aspects of the experimental design missing from the model? ####
 ####            Construct a model that includes any missing factors.
 
+## 1. construct model (again!!) ####
 
-## make individual-level random effect to use for overdispersed poisson ####
-d1$obs <- 1:length(d1$y) ## makes a unique number for each row in dataset
+r3 <- glmmTMB(y ~ spray * lead + (1|block), data=d1, family="nbinom2")   ### nb w/ block
 
-r3 <- glmmTMB(y ~ spray * lead + (1|obs), data=d1, family="poisson")     ### overdispersed poisson glm
-r4 <- glmmTMB(y ~ spray * lead + (1|block), data=d1, family="nbinom2")   ### nb w/ block
-r5 <- glmmTMB(y ~ spray * lead + (1|obs) + (1|block), data=d1, family="poisson")   ### OD poisson w/ block
-
-#### Six models above differ only distribution and random effects. Fixed effects are the same.
-#### Can use AIC for selecting the most appropriate distribution and random effects.
+#### Two models (r2 and r3) above differ only random effects. Fixed effects are the same.
+#### Can use AIC for selecting the most appropriate random effects.
 #### for model selection, use AIC or likihood ratio test
 #### Note that I've included the models with the block. Because 'block' was part of the expeirment, it should be in the model no matter what.
-r0 <- glmmTMB(y ~ spray * lead, data=d1) # normal model just for comparison
 
-model.sel(r0,r1,r2,r3,r4,r5) ## from MuMIn
-AICtab(r0,r1,r2,r3,r4,r5,base=T,logLik=T,weights=T)    ## from bbmle
+## 2. examine residuals ####
+plot(simulateResiduals(r3)) ## DHARMa package simulated residuals
+hist(simulateResiduals(r3)) ## histogram should be flat
+check_overdispersion(r3) # overdispersion ratio calculator from performance
 
-#### now you can go on to examine residuals, fixed effects using Anova(), emmeans, etc.
-plot(simulateResiduals(r4)) ## says levene's test is significant but the boxes look pretty good
-hist(simulateResiduals(r4)) ## histogram should be flat
+## 3. Check random effects
+summary(r3)
 
-plot(simulateResiduals(r5)) ## resids look great
-hist(simulateResiduals(r5)) ## histogram should be flat
+library(broom.mixed)
+glance(r3)
+tidy(r3, effects="ran_pars") ## same as stdev in summary()
+r3_blups <- tidy(r3, effects="ran_vals") ## same as stdev in summary()
+hist(r3_blups$estimate, breaks=seq(-.8,.8,l=6), main="Hist of RE blups")
 
-#### Look at variance components and block blups
+
+### 4. can compare models with and without random effects ####
+## If the random effect was part of your experimental design you don't need to do this (just keep block in!)
+model.sel(r2,r3) ## from MuMIn
+AICtab(r1,r2,r3,base=T,logLik=T,weights=T)    ## from bbmle
+
+## 4. Check significance ####
+Anova(r3)
+
+Anova(r2) ## compare with no-block model, similar in this case
+
+
+## 5. Calculate emmeans ####
+  # 6. constrasts
+emmeans(r3, pairwise ~ spray:lead, type="response")
+
+## 7. Calculate R2m and R2c
+r2(r3)
+r.squaredGLMM(r3) ## from MuMIn, lognormal is same as performance
+
+r.squaredGLMM(r2) ## compare to no-block model
+
+
+#### EXTRA - Look at variance components and block blups
 library(broom.mixed)
 glance(r4)
-r4blups <- tidy(r4, effects="ran_vals")
+r3blups <- tidy(r3, effects="ran_vals")
 
-ggplot(r4blups, aes(x=level, y=estimate, ymin=estimate-std.error, ymax=estimate+std.error))+
+ggplot(r3blups %>% group_by(level) %>% summarize(blup=mean(estimate),std.error=mean(std.error)) %>% 
+         arrange(blup) %>% mutate(Block=factor(level, levels=level)), 
+       aes(x=Block, y=blup, ymin=blup-std.error, ymax=blup+std.error))+
   geom_pointrange()+
   geom_hline(aes(yintercept=0), lty="dashed")+
   theme_bw(base_size = 16)+
