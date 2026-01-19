8a47f6c9a9d517e0cf64e4355039653602789373
Hahn <hahnp@ufl.edu>
2026-01-19 12:21:20 -0500

    updated wald chisq file

--- Wald chisq example.R
+++ Wald chisq example.R
@@ -18,7 +18,7 @@ ggplot(df, aes(x=group, y=value))+
   theme_bw(base_size = 20)
 
 
-modelf <- aov(value ~ group, data=df)
+modelf <- lm(value ~ group, data=df)
 summary(modelf)
 Anova(modelf)
 
@@ -27,18 +27,171 @@ summary(modelx2)
 Anova(modelx2)
 
 
-
-
-2^2/sd(df$value)
-
-model <- glmmTMB(value ~ group, data=df)
-summary(model)
-Anova(model, test = "Chisq")
-
 x2 <- modelx2 <- (2/.6667)^2
 # p-value 
 1-pchisq(x2, 1)
 
+
+## calucate difference using likelihood manually 
+one <- c(2, 3, 4)
+two <- c(4, 5, 6)
+log_likelihood <- function(par, y1, y2) {
+  mu1 <- par[1]
+  mu2 <- par[2]
+  sigma <- exp(par[3])  # enforce positivity
+  
+  ll1 <- sum(dnorm(y1, mean = mu1, sd = sigma, log = TRUE))
+  ll2 <- sum(dnorm(y2, mean = mu2, sd = sigma, log = TRUE))
+  
+  ll1 + ll2
+}
+
+start <- c(
+  mu1 = mean(one),
+  mu2 = mean(two),
+  log_sigma = log(sd(c(one, two)))
+)
+
+# optimioze
+fit <- optim(
+  par = start,
+  fn = log_likelihood,
+  y1 = one,
+  y2 = two,
+  control = list(fnscale = -1),  # maximize
+  hessian = TRUE
+)
+
+
+mu1_hat <- round(fit$par[1], 3)
+mu2_hat <- round(fit$par[2], 3)
+sigma_hat <- round(exp(fit$par[3]), 3)
+
+delta_hat <- mu2_hat - mu1_hat
+
+mu1_hat
+mu2_hat
+delta_hat
+
+# extract SE
+vcov_mat <- solve(fit$hessian)
+vcov_mat
+var_delta <- vcov_mat[2, 2] +
+  vcov_mat[1, 1] -
+  2 * vcov_mat[1, 2]
+
+se_delta <- sqrt(abs(var_delta)) # need abs here for non-postive definite hessian due to small sample size
+se_delta
+
+# z test
+z_val <- round((delta_hat / se_delta), 3)
+
+p_valz <- 2 * (1 - pnorm(z_val))
+p_valz
+
+x2_val <- z_val^2
+p_valx2 <- pchisq(x2_val, 1, lower.tail=F)
+p_valx2
+
+## visualize normal density
+# Create a sequence of x values
+x <- seq(0, 8, length.out = 400)
+
+# Parameters
+mu1 <- 3
+mu2 <- 5
+sd  <- 1.140175
+
+# Compute densities
+dens1 <- dnorm(x, mean = mu1, sd = sd)
+dens2 <- dnorm(x, mean = mu2, sd = sd)
+
+# Plot first normal density
+ll1p <- (dnorm(y1, mean = mu1, sd = sd, log = F))
+ll2p <- (dnorm(y2, mean = mu2, sd = sd, log = F))
+plot(x, dens1,
+     type = "l",
+     lwd = 2,
+     xlab = "group one",
+     ylab = "Density",
+     main = "Normal Density (mean = 3, sd = 4.86)")
+points(x=y1, y=ll1p, cex=2)
+
+# Plot second normal density
+plot(x, dens2,
+     type = "l",
+     lwd = 2,
+     xlab = "group two",
+     ylab = "Density",
+     main = "Normal Density (mean = 5, sd = 4.86)")
+points(x=y2, y=ll2p, cex=2)
+
+
+## visualize likelihood function
+delta_grid <- seq(0, 4, length.out = 200)
+
+profile_ll <- sapply(delta_grid, function(d) {
+  mu1 <- mean(one)
+  mu2 <- mu1 + d
+  sigma <- sd(c(one, two))
+  
+  sum(dnorm(one, mu1, sigma, log = TRUE)) +
+    sum(dnorm(two, mu2, sigma, log = TRUE))
+})
+
+plot(delta_grid, profile_ll,
+     type = "l",
+     xlab = "Difference in means (mu2 - mu1)",
+     ylab = "Log-likelihood",
+     main = "Likelihood for difference in means")
+
+
+## visualizing z 
+x <- seq(-4, 4, length.out = 500)
+y <- dt(x, df = 1)
+t_crit <- qt(1 - .05/2, df = 4)
+
+plot(x, y, type = "l",
+     main = "z distribution",
+     xlab = "z value",
+     ylab = "Density")
+
+# Observed statistic
+abline(v = 3, col = "red", lwd = 2)
+
+abline(v = c(-t_crit, t_crit), lty = 2) # crit values
+
+abline(v = z_val, col = "red", lwd = 2)
+#abline(v = -z_val, col = "red", lwd = 2, lty = 2)
+
+
+## chi sq
+x <- seq(0, 10, length.out = 500)
+y <- dchisq(x, df = 1)
+chi_crit <- qchisq(1 - 0.05, df = 1)
+
+plot(x, y, type = "l",
+     main = expression(chi^2~"distribution (df = 1)"),
+     xlab = "chi-sq value",
+     ylab = "Density")
+
+abline(v = chi_crit, lty = 2)
+abline(v = x2_val, col = "red", lwd = 2)
+
+## F distribution
+x <- seq(0, 10, length.out = 500)
+y <- df(x, df1 = 1, df2 = 4)
+F_crit <- qf(1 - 0.05, df1 = 1, df2 = 4)
+
+plot(x, y, type = "l",
+     main = "F distribution (df = 1,4)",
+     xlab = "F value",
+     ylab = "Density")
+
+abline(v = F_crit, lty = 2)
+abline(v = 6, col = "red", lwd = 2)
+
+1-pf(6, 1, 4) # pval from F test
 
 ## three groups
 df3 <- data.frame(
